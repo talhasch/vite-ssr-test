caec8869dab5987b11e5f9b6b92d6a39afb1c778
talhasch <talhabugrabulut@gmail.com>
2023-10-17 15:30:36 +0300

    sav

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const app = require('express')();
 
 app.get('/u/:username', (req, res) => {
-  const index = fs.readFileSync('dist/index.html', 'utf-8');
+  const index = fs.readFileSync(path.join(process.cwd(), 'dist', 'index.html'), 'utf-8');
   res.send(index);
 });
 
