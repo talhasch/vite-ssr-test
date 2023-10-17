143e5bbdca44b5c8b0267143105785cceff44b2d
talhasch <talhabugrabulut@gmail.com>
2023-10-17 14:50:10 +0300

    sav

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const app = require('express')();
 
-app.get('*', function (req, res) {
-  // const index = fs.readFileSync('./build/index.html', 'utf-8');
-  // res.send(index);
-
-  res.send('Express on Vercel');
+app.get('/api', (req, res) => {
+  res.send('hello from express');
 });
 
 module.exports = app;
