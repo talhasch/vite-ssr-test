8d4cb82a19b8fd0ddb3fafe71f147cf15b5acd79
talhasch <talhabugrabulut@gmail.com>
2023-10-17 15:49:25 +0300

    sav

--- api/index.js
+++ api/index.js
@@ -5,12 +5,10 @@ const app = require('express')();
 app.get('/u/:username', (req, res) => {
   const index = fs.readFileSync(path.join(process.cwd(), 'dist', 'index.html'), 'utf-8');
   const {username} = req.params;
-  const tags = `
-    <!-- meta -->
+  const tags = `<!-- meta -->
     <title>${username}'s page</title>
     <meta name="description" content="All about ${username}">
-    <!-- /meta -->
-  `;
+    <!-- /meta -->`;
   const modified = index.replace(/<!-- meta -->((.|\n|r)+)<!-- \/meta -->/m, tags);
   res.send(modified);
 });
