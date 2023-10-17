875a2a4064d51cc2364c8a4fc6d176a7c339e3d9
talhasch <talhabugrabulut@gmail.com>
2023-10-17 15:34:08 +0300

    sav

--- api/index.js
+++ api/index.js
@@ -5,7 +5,11 @@ const app = require('express')();
 app.get('/u/:username', (req, res) => {
   const index = fs.readFileSync(path.join(process.cwd(), 'dist', 'index.html'), 'utf-8');
   const {username} = req.params;
-  res.send(username);
+  const tags = `
+    <title>${username}'s page</title>
+  `;
+  const modified = index.replace(/<!-- meta -->(.*)<!-- \/meta -->/, tags);
+  res.send(index);
 });
 
 module.exports = app;
