903ae3f7a03dc286039b395b3fbea12f6980e9a4
talhasch <talhabugrabulut@gmail.com>
2023-10-17 15:28:54 +0300

    sav

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const app = require('express')();
 
 app.get('/u/:username', (req, res) => {
-  res.send('hello from express');
+  const index = fs.readFileSync('dist/index.html', 'utf-8');
+  res.send(index);
 });
 
 module.exports = app;
