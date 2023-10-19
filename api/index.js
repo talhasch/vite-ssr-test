ceb83b06105cb275aa189251d99c5781d1797abd
talhasch <talhabugrabulut@gmail.com>
2023-10-19 12:59:06 +0300

    save

--- api/index.js
+++ api/index.js
@@ -5,31 +5,30 @@ const app = express();
 
 const indexRaw = fs.readFileSync(path.join(process.cwd(), 'dist', 'index.html'), 'utf-8').toString();
 
+const TITLE_SELECTOR =  '//title-here//';
+const DESCRIPTION_SELECTOR = '//description-here//';
+const IMAGE_SELECTOR = '//image-here//';
+
 const indexManipulated = (() => {
-  const jsdom = require("jsdom");
+  const jsdom = require('jsdom');
   const { JSDOM } = jsdom;
   const indexDom = new JSDOM(indexRaw);
-  indexDom.window.document.querySelector('title').textContent = '//title-here//';
-  indexDom.window.document.querySelector('meta[name="description"]').setAttribute('content', '//description-here//');
+  indexDom.window.document.querySelector('title').textContent = TITLE_SELECTOR;
+  indexDom.window.document.querySelector('meta[name="description"]').setAttribute('content', DESCRIPTION_SELECTOR);
   const html = indexDom.serialize();
   indexDom.window.close();
   return html;
 })();
 
-console.log(indexManipulated);
+const preparePage = (title, description, image) => {
+  return indexManipulated.replace(TITLE_SELECTOR, title).replace(DESCRIPTION_SELECTOR, description).replace(IMAGE_SELECTOR, image);
+}
 
 
 app.get('/u/:username', (req, res) => {
-  /*
-  const index = fs.readFileSync(path.join(process.cwd(), 'dist', 'index.html'), 'utf-8').toString();
   const { username } = req.params;
-  const tags = `<!-- meta -->
-    <title>${username}'s page</title>
-    <meta name="description" content="All about ${username}">
-    <!-- /meta -->`;
-  const modified = index.replace(/<!-- meta -->((.|\n|r)+)<!-- \/meta -->/m, tags);
-  */
-  res.send(indexManipulated);
+  const html = preparePage(`@${username}'s page`, `All about ${username}`, '');
+  res.send(html);
 });
 
 module.exports = app;
